406f65a8b35f3f93892e368b7597f6e7cf0d4587
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-05-08 14:10:13 +0000

    lkjbvghuioiuhvghij

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,43 @@
-public class PracticeProblem {
+class Circle{
+	public double radius;
 
-	public static void main(String args[]) {
-
+	public Circle(double radius){
+		this.radius = radius;
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public double diameter(){
+		return this.radius*2;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public double perimeter(){
+		return Math.PI*this.radius*2;
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public double area(){
+		return Math.PI*Math.pow(this.radius, 2);
 	}
 
-	public static void q4() {
-		//Write question 4 code here
+	public void setRadius(double radius){
+		this.radius = radius;
 	}
 
-	public static void q5() {
-		//Write question 5 code here
+	public double getRadius(){
+		return this.radius;
+	}
+}
+
+class Sphere extends Circle{
+
+	public Sphere(double radius){
+		super(radius);
 	}
 
+	@Override
+	public double area(){
+		return 4*Math.PI*Math.pow(radius, 2);
+	}
+
+	public double volume(){
+		return (4.0/3.0)*Math.PI*Math.pow(radius, 3);
+	}
 }
